3332feda4b182a309f523402d67c7c2bb51e5962
Jeremy Schulman <jeremy.schulman@mlb.com>
2020-10-07 15:22:05 -0400

    update

--- netcfg_grep/cli/cli.py
+++ netcfg_grep/cli/cli.py
@@ -12,15 +12,15 @@ VERSION = metadata.version("netcfg-grep")
 @click.command()
 @click.version_option(version=VERSION)
 @click.option(
-    "-c", "--config", help="netcfg-grep configuration YAML file", type=click.File()
+    "-g", "--grep-config", help="netcfg-grep configuration YAML file", type=click.File()
 )
 @click.option(
-    "-f",
-    "--device-config-file",
+    "-d",
+    "--device-config",
     help="network device configuration TEXT file",
     type=click.File(),
 )
-def cli(config, device_config_file):
-    ncg_config = yaml.safe_load(config)
-    results_list = grep(ncg_config=ncg_config, netcfg_filepath=device_config_file.name)
+def cli(grep_config, device_config):
+    ncg_config = yaml.safe_load(grep_config)
+    results_list = grep(ncg_config=ncg_config, netcfg_filepath=device_config.name)
     print("\n\n".join(results_list))
